e2a2e5af5ba9d8fb50b2b8a3c6ba391a067f3e4f
tyler <tyler.reitz@protonmail.com>
2025-11-11 21:27:44 +0000

    CH2: Database; L2: Drizzle ORM

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'drizzle-kit'
+import { readConfig } from './src/config'
+
+export default defineConfig({
+  schema: 'src/lib/db/schema.ts',
+  out: 'src/lib/db/dist',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: readConfig()?.dbUrl
+  }
+})
+
--- src/config.ts
+++ src/config.ts
@@ -1,35 +1,37 @@
-import fs from "fs"
-import os from "os"
-import path from "path"
+import fs from "fs";
+import os from "os";
+import path from "path";
 
 export type Config = {
   dbUrl: string;
   currentUserName: string;
-}
+};
 
 export function setUser(currentUserName: string): void {
-  writeConfig({...readConfig(), currentUserName})
+  writeConfig({ ...readConfig(), currentUserName });
 }
 
 export function readConfig(): Config {
-  const configJSON = fs.readFileSync(getConfigFilePath(), {encoding: 'utf8'})
-  return validateConfig(configJSON)
+  const configJSON = fs.readFileSync(getConfigFilePath(), { encoding: "utf8" });
+  return validateConfig(configJSON);
 }
 
 function getConfigFilePath(): string {
-  return path.join(os.homedir(), '.gatorconfig.json')
+  return path.join(os.homedir(), ".gatorconfig.json");
 }
 
 function writeConfig(cfg: Config): void {
-  const { dbUrl: db_url, currentUserName: current_user_name } = cfg
-  fs.writeFileSync(getConfigFilePath(), JSON.stringify({db_url, current_user_name}), {encoding: 'utf8'})
+  const { dbUrl: db_url, currentUserName: current_user_name } = cfg;
+  fs.writeFileSync(
+    getConfigFilePath(),
+    JSON.stringify({ db_url, current_user_name }, null, 2),
+    { encoding: "utf8" },
+  );
 }
 
 function validateConfig(rawConfig: any): Config {
-  const {
-    db_url: dbUrl,
-    current_user_name: currentUserName
-  } = JSON.parse(rawConfig)
+  const { db_url: dbUrl, current_user_name: currentUserName } =
+    JSON.parse(rawConfig);
 
-  return { dbUrl, currentUserName }
+  return { dbUrl, currentUserName };
 }
